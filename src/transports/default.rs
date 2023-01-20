9df2f28488df9904a502d369660da1a51a901a75
dzdidi <deniszalessky@gmail.com>
2023-01-20 11:06:40 +0100

    transport: default - flow draft
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/transports/default.rs
+++ src/transports/default.rs
@@ -3,3 +3,9 @@
 // handled by corresponding git library.
 // TODO: simple wrapper for client and bare server to be able to access basic push/pull operations
 // via this code base
+//
+// add  to gitignore /${dir}.git
+// git  clone --bare ${dir} ${dir}.git
+// make a symlink to jailed directory (git clone works with path)
+// create a restricted user with access only to jailed directory
+// add counterparty public key to the restricted git user
